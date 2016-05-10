169518584bef90dfb2c21d5ac9b1510991552604
imoto-yuya-1234 <imoto.yuya.1234@gmail.com>
2016-05-10 22:09:39 +0900

    version 2.0

--- src/main.c
+++ src/main.c
@@ -24,19 +24,19 @@ static void bg_update_proc(Layer *layer, GContext *ctx) {
 }
 
 static void ticks_update_proc(Layer *layer, GContext *ctx) {
-	GRect bounds = layer_get_bounds(layer);
-  GPoint center = grect_center_point(&bounds);
-	
 	int32_t angle;
 	int16_t length, width;
-	const int16_t radius = PBL_IF_ROUND_ELSE(bounds.size.w / 2, bounds.size.h / 2);
+	#if defined(PBL_ROUND) 
+	GRect bounds = layer_get_bounds(layer);
+  GPoint center = grect_center_point(&bounds);
+	const int16_t radius = bounds.size.w / 2;
 	for(int i = 0; i < 60; ++i) {
 		angle = TRIG_MAX_ANGLE * i / 60;
 		if(i % 5 == 0) {
-			length = PBL_IF_ROUND_ELSE(8, 8);
+			length = 8;
 			width = 3;
 		} else {
-			length = PBL_IF_ROUND_ELSE(4, 4);
+			length = 4;
 			width = 1;
 		}
 		GPoint start = {
@@ -51,6 +51,65 @@ static void ticks_update_proc(Layer *layer, GContext *ctx) {
 		graphics_context_set_stroke_width(ctx, width);
 		graphics_draw_line(ctx, start, end);
 	}
+	#elif defined(PBL_RECT)
+	for(int j = 0; j < 60; ++j) {
+		angle = TRIG_MAX_ANGLE * j / 60;
+		if(j % 5 == 0) {
+			length = 8;
+			width = 3;
+		} else {
+			length = 4;
+			width = 1;
+		}
+		//GPoint start, end;
+		graphics_context_set_stroke_color(ctx, GColorWhite);
+		graphics_context_set_stroke_width(ctx, width);
+		if(j < 7 || j >= 54) {
+			GPoint start = {
+				.x = 72 + (int16_t)(84 * sin_lookup(angle) / cos_lookup(angle)), 
+				.y = 0,
+			};
+			GPoint end = {
+				.x = 72 + (int16_t)((84 - length) * sin_lookup(angle) / cos_lookup(angle)), 
+				.y = length,
+			};
+			graphics_draw_line(ctx, start, end);
+		}
+		else if(j < 24) {
+			GPoint start = {
+				.x = 144, 
+				.y = 84 - (int16_t)(72 * cos_lookup(angle) / sin_lookup(angle)),
+			};
+			GPoint end = {
+				.x = 144 - length, 
+				.y = 84 - (int16_t)((72 - length) * cos_lookup(angle) / sin_lookup(angle)),
+			};
+			graphics_draw_line(ctx, start, end);
+		}
+		else if(j < 37) {
+			GPoint start = {
+				.x = 72 - (int16_t)(84 * sin_lookup(angle) / cos_lookup(angle)), 
+				.y = 168,
+			};
+			GPoint end = {
+				.x = 72 - (int16_t)((84 - length) * sin_lookup(angle) / cos_lookup(angle)), 
+				.y = 168 - length,
+			};
+			graphics_draw_line(ctx, start, end);
+		}
+		else if(j < 54) {
+			GPoint start = {
+				.x = 0, 
+				.y = 84 + (int16_t)(72 * cos_lookup(angle) / sin_lookup(angle)),
+			};
+			GPoint end = {
+				.x = length, 
+				.y = 84 + (int16_t)((72 - length) * cos_lookup(angle) / sin_lookup(angle)),
+			};
+			graphics_draw_line(ctx, start, end);
+		}
+	}
+	#endif
 }
 
 static void hands_update_proc(Layer *layer, GContext *ctx) {
@@ -135,11 +194,48 @@ static void date_update_proc(Layer *layer, GContext *ctx) {
 
 static void battery_update_proc(Layer *layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(layer);
+	
+	#if defined(PBL_ROUND) 
   int32_t angle_start = DEG_TO_TRIGANGLE(0);
 	int32_t angle_end = DEG_TO_TRIGANGLE((int32_t)(360 * s_battery_level / 100));
-	
 	graphics_context_set_fill_color(ctx, GColorIslamicGreen);
 	graphics_fill_radial(ctx, bounds, GOvalScaleModeFitCircle, 8, angle_start, angle_end);
+	#elif defined(PBL_RECT)
+	int32_t decre_length = (int32_t)(604 - 604 * s_battery_level / 100);
+	int32_t decre_1 = 0, decre_2 = 0, decre_3 = 0, decre_4 = 0, decre_5 = 0;
+	graphics_context_set_fill_color(ctx, GColorIslamicGreen);
+	graphics_context_set_stroke_color(ctx , GColorIslamicGreen);
+	if(decre_length < 67) {
+		decre_1 = decre_length;
+	}
+	else if(decre_length < 230) {
+		decre_1 = 67;
+		decre_2 = decre_length - 67;
+	}
+	else if(decre_length < 373) {
+		decre_1 = 67;
+		decre_2 = 163;
+		decre_3 = decre_length - 230;
+	}
+	else if(decre_length < 536) {
+		decre_1 = 67;
+		decre_2 = 163;
+		decre_3 = 143;
+		decre_4 = decre_length - 373;
+	}
+	else if(decre_length <=  604) {
+		decre_1 = 67;
+		decre_2 = 163;
+		decre_3 = 143;
+		decre_4 = 163;
+		decre_5 = decre_length - 536;
+	}
+	graphics_fill_rect(ctx, GRect(5, 0, 67 - decre_1, 5), 0, GCornerNone);
+	graphics_fill_rect(ctx, GRect(0, decre_2, 5, 163 - decre_2), 0, GCornerNone);
+	graphics_fill_rect(ctx, GRect(decre_3, 163, 139 - decre_3, 5), 0, GCornerNone);
+	graphics_fill_rect(ctx, GRect(139, 5, 5, 163 - decre_4), 0, GCornerNone);
+	graphics_fill_rect(ctx, GRect(72, 0, 72 - decre_5, 5), 0, GCornerNone);
+	#endif
 }
 
 static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
